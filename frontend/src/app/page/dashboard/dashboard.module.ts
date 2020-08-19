619644a6cb448ce9fcce067e581de1648eeeb529
supermanIT <supermanit0517@gmail.com>
2020-08-19 17:09:15 +0800

    change dashboard

--- frontend/src/app/page/dashboard/dashboard.module.ts
+++ frontend/src/app/page/dashboard/dashboard.module.ts
@@ -5,15 +5,17 @@ import {DashboardComponent} from './dashboard.component';
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
 import {MatMenuModule} from "@angular/material/menu";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [
-    CommonModule,
-    DashboardRoutingModule,
-    MatIconModule,
-    MatButtonModule,
-    MatMenuModule,
-  ]
+    imports: [
+        CommonModule,
+        DashboardRoutingModule,
+        MatIconModule,
+        MatButtonModule,
+        MatMenuModule,
+        RouterModule,
+    ]
 })
 export class DashboardModule { }
