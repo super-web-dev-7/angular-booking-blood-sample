516dcc779dd3fd668f54d5d6a3b1d96bfdf8fbd3
supermanIT <supermanIT0517@gmail.com>
2020-10-07 23:22:38 +0800

    add zipcode model

--- backend/src/models/zipcode.model.js
+++ backend/src/models/zipcode.model.js
@@ -1,10 +1,15 @@
 module.exports = (sequelize, Sequelize) => {
-    return sequelize.define('zipcode_model', {
-        ort: {
-            type: Sequelize.STRING,
+    return sequelize.define('zipcode_model',
+        {
+            ort: {
+                type: Sequelize.STRING,
+            },
+            plz: {
+                type: Sequelize.INTEGER
+            }
         },
-        plz: {
-            type: Sequelize.INTEGER
-        }
-    });
+        {
+            charset: 'utf8',
+            collate: 'utf8_unicode_ci'
+        });
 };
