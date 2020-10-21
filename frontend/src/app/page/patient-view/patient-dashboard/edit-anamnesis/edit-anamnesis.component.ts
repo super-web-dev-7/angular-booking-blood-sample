f57051ccebaa28df4bc661d5a1585ac849b0596d
supermanIT <supermanIT0517@gmail.com>
2020-10-21 15:49:18 +0800

    integrate new medical question with api in patient view

--- frontend/src/app/page/patient-view/patient-dashboard/edit-anamnesis/edit-anamnesis.component.ts
+++ frontend/src/app/page/patient-view/patient-dashboard/edit-anamnesis/edit-anamnesis.component.ts
@@ -1,6 +1,8 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {HttpService} from '../../../../service/http/http.service';
+import {URL_JSON} from '../../../../utils/url_json';
 
 @Component({
   selector: 'app-edit-anamnesis',
@@ -13,9 +15,12 @@ export class EditAnamnesisComponent implements OnInit {
   saved = false;
   public districtSearchControl = new FormControl();
   allStaticDistrict = [];
-  ageRange = new Array(65);
-  heightRange = new Array(70);
-  weightRange = new Array(100);
+  ageRange = new Array(81);
+  heightRange = new Array(140);
+  weightRange = new Array(265);
+  diseases = [
+    'Gegen Diabetes', 'Gegen Bluthochdruck', 'Schilddrüsenüberfunktion', 'Schilddrüsenunterfunktion'
+  ];
   selectValues = {
     smoking: true,
     alcohol: false,
@@ -28,14 +33,16 @@ export class EditAnamnesisComponent implements OnInit {
   constructor(
     public formBuilder: FormBuilder,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private dialogRef: MatDialogRef<EditAnamnesisComponent>
-  ) { }
+    private dialogRef: MatDialogRef<EditAnamnesisComponent>,
+    public httpService: HttpService
+  ) {
+  }
 
   ngOnInit(): void {
+    this.saved = false;
     this.editAnamsForm = this.formBuilder.group({
       name: [null, Validators.required],
       gender: [null, Validators.required],
-      male: [null, Validators.required],
       age: [null, Validators.required],
       height: [null, Validators.required],
       weight: [null, Validators.required],
@@ -43,13 +50,30 @@ export class EditAnamnesisComponent implements OnInit {
     });
   }
 
+  get f(): any {
+    return this.editAnamsForm.controls;
+  }
+
   submit = () => {
-    this.saved = true;
-    console.log(this.selectValues);
+    if (this.editAnamsForm.invalid) {
+      return;
+    }
+    const data = {
+      name: this.f.name.value,
+      gender: this.f.gender.value,
+      age: this.f.age.value,
+      height: this.f.height.value,
+      weight: this.f.weight.value,
+      disease: this.f.disease.value,
+      ...this.selectValues,
+      ...this.data
+    };
+    this.httpService.post(URL_JSON.PATIENT + '/createMedicalQuestion', data).subscribe((res: any) => {
+      this.saved = true;
+    });
   }
 
   close = () => {
     this.dialogRef.close(false);
   }
-
 }
