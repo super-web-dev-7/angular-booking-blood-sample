dd8288c350aff20a784ef3ee28902d486681dcf8
flyemirate0716 <flyemirate0716@gmail.com>
2020-10-21 12:59:38 +0530

    fix select package style issues

--- frontend/src/app/page/patient-view/patient-dashboard/callback-doctor/callback-doctor.component.ts
+++ frontend/src/app/page/patient-view/patient-dashboard/callback-doctor/callback-doctor.component.ts
@@ -7,16 +7,28 @@ import {MatDialogRef} from '@angular/material/dialog';
   styleUrls: ['./callback-doctor.component.scss']
 })
 export class CallbackDoctorComponent implements OnInit {
-
+  selectedDay: any;
+  times = [
+    {time: 'morning', label: 'Vormittags (09:00 - 12:00)'},
+    {time: 'noon', label: 'Nachmittags (12:00 - 16:00)'},
+    {time: 'evening', label: 'Abends (16:00 - 19:00)'}
+  ];
+  selectedTime = null;
+  isValid = false;
   constructor(
     private dialogRef: MatDialogRef<CallbackDoctorComponent>
   ) { }
 
   ngOnInit(): void {
+    this.selectedDay = null;
   }
 
   close = () => {
     this.dialogRef.close(false);
+  }
+
+  selectDay = (event) => {
+    this.selectedDay = event;
   }
 
 }
