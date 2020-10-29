87d1887231840431a1ae08b0e33b7647a93a7b0e
flyemirate0716 <flyemirate0716@gmail.com>
2020-10-30 01:18:06 +0530

    cancel appointment

--- frontend/src/app/page/patient-view/patient-dashboard/cancel-appointment/cancel-appointment.component.ts
+++ frontend/src/app/page/patient-view/patient-dashboard/cancel-appointment/cancel-appointment.component.ts
@@ -1,9 +1,11 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material/dialog';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import * as moment from 'moment';
 import {HttpService} from '../../../../service/http/http.service';
 import {URL_JSON} from '../../../../utils/url_json';
+import {AuthService} from '../../../../service/auth/auth.service';
+import {DialogSuccessComponent} from '../dialog-success/dialog-success.component';
 
 @Component({
   selector: 'app-cancel-appointment',
@@ -15,14 +17,18 @@ export class CancelAppointmentComponent implements OnInit {
   success = false;
   cancelForm: FormGroup;
   displayData: any;
+  currentUser: any;
   constructor(
     public formBuilder: FormBuilder,
     @Inject(MAT_DIALOG_DATA) public data: any,
     private dialogRef: MatDialogRef<CancelAppointmentComponent>,
     public httpService: HttpService,
+    public authService: AuthService,
+    public dialog: MatDialog,
   ) { }
 
   ngOnInit(): void {
+    this.currentUser = this.authService.currentUserValue;
     this.showMessage = false;
     this.httpService.get(URL_JSON.APPOINTMENT + '/getAppointmentDetail/' + this.data.appointmentId).subscribe((res: any) => {
       this.displayData = res;
@@ -47,6 +53,19 @@ export class CancelAppointmentComponent implements OnInit {
     if (this.cancelForm.invalid) {
       return;
     }
+    const data = {
+      appointmentId: this.data?.appointmentId,
+      userId: this.currentUser.id,
+      content: this.f.message.value
+    };
+    this.httpService.post(URL_JSON.PATIENT + '/cancel_appointment_by_patient', data).subscribe((res: any) => {
+      if (res) {
+        const dialogRef = this.dialog.open(DialogSuccessComponent, {
+          width: '662px'
+        });
+        dialogRef.close();
+      }
+    });
   }
 
   get f(): any {
