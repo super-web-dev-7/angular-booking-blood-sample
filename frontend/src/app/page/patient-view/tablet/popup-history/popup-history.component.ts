93f5f15cc8e93b72cf6be1fed8a1a0a4782ed30f
flyemirate0716 <flyemirate0716@gmail.com>
2020-11-01 00:00:05 +0530

    contact history mobile and tablet

--- frontend/src/app/page/patient-view/tablet/popup-history/popup-history.component.ts
+++ frontend/src/app/page/patient-view/tablet/popup-history/popup-history.component.ts
@@ -1,4 +1,7 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {HttpService} from '../../../../service/http/http.service';
+import {URL_JSON} from '../../../../utils/url_json';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-popup-history',
@@ -9,9 +12,34 @@ export class PopupHistoryComponent implements OnInit {
   @Output() closeSide = new EventEmitter();
   @Input() isMobile;
   @Input() isTablet;
-  constructor() { }
+  @Input() appointmentId;
+  historyData: any;
+  appointmentData: any;
+  constructor(
+    public httpService: HttpService,
+  ) { }
 
   ngOnInit(): void {
+    this.httpService.get(URL_JSON.DOCTOR + '/getContactHistory/' + this.appointmentId).subscribe((res: any) => {
+      if (res) {
+        this.appointmentData = res.appointment[0];
+        this.historyData = res.contactHistory;
+      }
+    });
+  }
+
+  getTimeDuration = (startTime, duration) => {
+    if (!startTime || !duration) {
+      return '';
+    }
+    return moment(startTime).format('DD.MM.YYYY HH:mm') + ' - ' + moment(startTime + duration * 60 * 1000).format('HH:mm');
+  }
+
+  formatTime = (time) => {
+    if (!time) {
+      return '';
+    }
+    return moment(time).format('DD.MM.YYYY HH:mm');
   }
 
   close = () => {
