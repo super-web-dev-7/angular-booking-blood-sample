4bb53c91b343a38d0005f29a81efa192b6af4390
flyemirate0716 <flyemirate0716@gmail.com>
2020-10-31 23:32:17 +0530

    cancel, shift schedule tablet and mobile

--- frontend/src/app/page/patient-view/tablet/popup-cancel-appointment/popup-cancel-appointment.component.ts
+++ frontend/src/app/page/patient-view/tablet/popup-cancel-appointment/popup-cancel-appointment.component.ts
@@ -1,7 +1,10 @@
 import {Component, EventEmitter, HostListener, Input, OnInit, Output} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {BreakpointObserver} from '@angular/cdk/layout';
-import {MatDialogRef} from '@angular/material/dialog';
+import {HttpService} from '../../../../service/http/http.service';
+import {AuthService} from '../../../../service/auth/auth.service';
+import {URL_JSON} from '../../../../utils/url_json';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-popup-cancel-appointment',
@@ -13,17 +16,26 @@ export class PopupCancelAppointmentComponent implements OnInit {
   cancelForm: FormGroup;
   showMessage: boolean;
   success = false;
+  @Input() appointmentId;
   @Input() isMobile;
   @Input() isTablet;
+  currentUser: any;
+  displayData: any;
   constructor(
     public formBuilder: FormBuilder,
     public breakpointObserver: BreakpointObserver,
+    public httpService: HttpService,
+    public authService: AuthService,
   ) { }
 
   ngOnInit(): void {
     this.showMessage = false;
+    this.currentUser = this.authService.currentUserValue;
+    this.httpService.get(URL_JSON.APPOINTMENT + '/getAppointmentDetail/' + this.appointmentId).subscribe((res: any) => {
+      this.displayData = res;
+    });
     this.cancelForm = this.formBuilder.group({
-      message: ['']
+      message: [null, Validators.required],
     });
   }
 
@@ -35,7 +47,27 @@ export class PopupCancelAppointmentComponent implements OnInit {
     this.showMessage = !this.showMessage;
   }
 
+  getTimeDuration = (startTime, duration) => {
+    if (!startTime || !duration) {
+      return '';
+    }
+    return moment(startTime).format('DD.MM.YYYY HH:mm') + ' - ' + moment(startTime + duration * 60 * 1000).format('HH:mm');
+  }
+
   submit = () => {
+    if (this.cancelForm.invalid) {
+      return;
+    }
+    const data = {
+      appointmentId: this.appointmentId,
+      userId: this.currentUser.id,
+      content: this.f.message.value
+    };
+    this.httpService.post(URL_JSON.PATIENT + '/cancel_appointment_by_patient', data).subscribe((res: any) => {
+      if (res) {
+        console.log(res);
+      }
+    });
     this.success = true;
   }
 
--- frontend/src/app/page/patient-view/tablet/popup-shift-schedule/popup-shift-schedule.component.ts
+++ frontend/src/app/page/patient-view/tablet/popup-shift-schedule/popup-shift-schedule.component.ts
@@ -1,5 +1,9 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {URL_JSON} from '../../../../utils/url_json';
+import {HttpService} from '../../../../service/http/http.service';
+import {AuthService} from '../../../../service/auth/auth.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-popup-shift-schedule',
@@ -10,26 +14,92 @@ export class PopupShiftScheduleComponent implements OnInit {
   moveForm: FormGroup;
   @Input() isMobile;
   @Input() isTablet;
+  @Input() appointmentId;
   allTimes = [];
   selectedPTime = null;
-  isValid = false;
+  displayData: any;
+  currentUser: any;
+  addressData = null;
+  success = false;
 
   @Output() closeSide = new EventEmitter();
   constructor(
     public formBuilder: FormBuilder,
+    public httpService: HttpService,
+    public authService: AuthService
   ) { }
 
   ngOnInit(): void {
+    this.currentUser = this.authService.currentUserValue;
     this.moveForm = this.formBuilder.group({
-      plz: [null, Validators.required],
-      ort: [null, Validators.required],
-      street: [null, Validators.required],
-      message: ['', Validators.required]
+      plz: [null],
+      ort: [null],
+      street: [null],
+      message: [null]
+    });
+    this.httpService.get(URL_JSON.APPOINTMENT + '/getAppointmentDetail/' + this.appointmentId).subscribe((res: any) => {
+      this.displayData = res;
+      if (this.displayData) {
+        this.getBookingTime(this.displayData.packageId);
+      }
     });
   }
 
   get f(): any {
     return this.moveForm.controls;
+  }
+
+  getBookingTime = (id) => {
+    this.httpService.get(URL_JSON.BASE + '/booking_time/package/' + id).subscribe((res: any) => {
+      this.allTimes = res;
+    });
+  }
+
+  getTimeDuration = (startTime, duration) => {
+    if (!startTime || !duration) {
+      return '';
+    }
+    return moment(startTime).format('DD.MM.YYYY HH:mm') + ' - ' + moment(startTime + duration * 60 * 1000).format('HH:mm');
+  }
+
+  checkPostalCode = () => {
+    this.httpService.checkPostalCode(this.f.plz.value).subscribe((res: any) => {
+      if (!res) {
+        this.f.plz.setErrors(Validators.required);
+      } else {
+        this.f.plz.setErrors(null);
+        this.f.ort.setValue(res.city);
+        this.f.street.setValue(res.district);
+      }
+    });
+  }
+
+  submit = () => {
+    if (!this.selectedPTime) {
+      return;
+    }
+    if (this.f.plz.value && this.f.ort.value && this.f.street.value) {
+      this.addressData = {
+        differentPlace: true,
+        otherStreet: this.f.street.value,
+        otherCity: this.f.ort.value,
+        otherPostalCode: this.f.plz.value
+      };
+    } else {
+      this.addressData = null;
+    }
+    const data = {
+      appointmentId: this.appointmentId,
+      patientId: this.currentUser?.id,
+      content: this.f.message.value,
+      time: this.allTimes[this.selectedPTime],
+      address: this.addressData
+    };
+    this.httpService.post(URL_JSON.PATIENT + '/shift_appointment_by_patient', data).subscribe((res: any) => {
+      if (res) {
+        this.success = true;
+      }
+    });
   }
 
   close = () => {
